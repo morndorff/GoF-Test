caaf5f6076cd06be701a8385618ed056243f6361
Mark Orndorff <markorndorff@gmail.com>
2014-07-20 21:28:48 -0400

    Split function files

--- July20-Reviewing One Sample.R
+++ July20-Reviewing One Sample.R
@@ -1,6 +1,8 @@
 # A script illustrating problems with the one sample
 # test statistic for myts
 
+rm(list=ls())
+library(ggplot2)
 source("functions.R")
 set.seed(5)
 
@@ -14,3 +16,74 @@ new.perm.test(normx,qnorm,list(mean=0,sd=3), f=myts)
 new.perm.test(normx,qnorm,list(mean=0,sd=3800), f=myts)
 # If you run the above, you'll notice that still occasionally,
 # you'll get a non-zero p-value (kind of odd)
+
+normx <- rnorm(50,0,3)
+
+new.perm.test(normx, qt, list(df=3), f=myts)
+
+normx1 <- rnorm(100,0,3)
+
+new.perm.test(normx1, qt, list(df=3), f=myts)
+
+normx1 <- rnorm(200,0,3)
+
+new.perm.test(normx1, qt, list(df=3), f=myts)
+
+normx1 <- rnorm(500,0,3)
+
+new.perm.test(normx1, qt, list(df=3), f=myts)
+
+# Note that p-value increases as number of samples increases
+# major problem
+
+# An illustratio nof why this happens
+myts(rt(500,3), qt,3,do.plot=TRUE)
+myts(rt(500,3), qt,3,do.plot=TRUE)
+myts(rt(500,3), qt,3,do.plot=TRUE)
+myts(rt(500,3), qt,3,do.plot=TRUE)
+
+# Distribution of my ts under the null that F0=t(3)
+matx_t_3 <- matrix(rt(1000000,3),nrow=2000,ncol=500)
+dist_t_3 <- apply(matx_t_3,MARGIN=1, FUN=myts, y=qt,df=3)
+plot_t_3 <- hist(dist_t_3,breaks=20, main="Distribution of myts under F0=t(3)")
+plot(plot_t_3)
+
+qplot(dist_t_3)
+
+# distribution of myts when F=N(0,3) and the null is F0=t(3)
+matx_n_03_t_3 <- matrix(rnorm(1000000,0,3),nrow=2000,ncol=500)
+dist_n_03_t_3 <- apply(matx_n_03_t_3,MARGIN=1, FUN=myts, y=qt,df=3)
+plot_n_03_t_3 <- hist(dist_n_03_t_3,breaks=20, main="Distribution of myts under F0=t(3)")
+plot(plot_n_03_t_3)
+
+qplot(dist_n_03_t_3)
+
+x <- c(dist_t_3,dist_n_03_t_3)
+y <- rep(c("a","b"),each=length(dist_t_3))
+overlay_data <- data.frame(x,y)
+
+quantile(dist_n_03_t_3,probs=.95)
+quantile(dist_t_3,probs=.95)
+mean(dist_t_3)
+mean(dist_n_03_t_3)
+
+ggplot(data=overlay_data,aes(x=x)) +
+  geom_density(data=subset(overlay_data, y=='a'), fill="red", alpha=.2) + 
+  geom_density(data=subset(overlay_data, y=='b'), fill="blue", alpha=.2) +
+  coord_cartesian(xlim=c(0,5))
+
+# ggplot(overlay_data, aes(x=dist_t_3, fill=yy)) + geom_histogram(alpha=0.2, position="identity")
+# 
+# + theme_bw()
+
+
+
+overlay_plot
+overlay_plot + geom_histogram(aes(x=dist_n_03_t_3))
+# The distribution of myts (obviously) depends on the 
+# underlying null distribution
+# Many of the distributions are pretty interesting
+matx <- matrix(runif(1000000,min=0,max=2),nrow=2000,ncol=500)
+dist <- apply(matx,MARGIN=1, FUN=myts, y=qunif,min=0, max=2)
+plot_u_0_2 <- hist(dist,breaks=20, main="Distribution of myts under F0=U(0,2)")
+plot(plot_u_0_2)
--- functions.R
+++ functions.R
@@ -3,67 +3,8 @@ source("functions_out.R")
 source("functions_plot.R")
 source("functions_power.R")
 source("functions_string.R")
+source("functions_tstats.R")
 
-myts <- function(x, y, ..., interp = 4, do.plot = FALSE) {
-    # Computes maximum difference of quantiles Comments: OBC Using Linear Interpolation
-    # of the ECDF Recall: ECDF range is [1/n,1]. This is probably not realistic Args: x:
-    # A vector of observations for a R.V. (must be numeric) y: Either (1) Another vector
-    # of observations (two sample) (2) A quantile function such as qnorm (one sample)
-    # interp: method of interpolation used. For more details, see ?quantile do.plot:
-    # Creates a plot illustrating the statistic Returns: The value of the statistic
-    
-    # Error Handling
-    if (is.numeric(x) != TRUE) 
-        stop("x must be numeric")
-    
-    x <- sort(x)
-    lenx <- length(x)
-    # Two Sample Test
-    if (is.numeric(y)) {
-        leny <- length(y)
-        y <- sort(y)
-        # x1 and y1 are the quantile values
-        x1 <- seq(1/lenx, 1, 1/lenx)
-        y1 <- seq(1/leny, 1, 1/leny)
-        if (lenx == leny) {
-            # In the case of equal sample sizes take max of abs. val
-            z <- max(abs(y - x))
-            if (do.plot == TRUE) 
-                plot.ts.2sam(x, y, x1, y1, lenx, leny)
-        } else if (lenx > leny) {
-            # If there are unequal sample sizes, then interpolation is necessary
-            q1 <- quantile(x, probs = x1, type = interp)
-            q_inter <- approxfun(x1, q1, yleft = min(q1), yright = max(q1))
-            z <- max(abs(q_inter(y1) - y))
-            if (do.plot == TRUE) 
-                plot.ts.2sam(x, y, x1, y1, lenx, leny)
-        } else {
-            # So length y>x
-            q2 <- quantile(y, probs = y1, type = interp)
-            q_inter <- approxfun(y1, q2, yleft = min(q2), yright = max(q2))
-            z <- max(abs(q_inter(x1) - x))
-            if (do.plot == TRUE) 
-                plot.ts.2sam(x, y, x1, y1, lenx, leny)
-        }
-        return(z)
-    }
-    
-    # One Sample
-    if (is.list(y)) 
-        y <- names(y)
-    if (is.function(y)) 
-        funname <- as.character(substitute(y))
-    if (is.character(y)) 
-        funname <- y
-    y <- get(funname, mode = "function", envir = parent.frame())
-    if (!is.function(y)) 
-        stop("'y' must be numeric or a function or a string naming a valid function")
-    z <- max(abs(x - y(seq(1/(lenx + 1), lenx/(lenx + 1), length.out = lenx), ...)))  #Note: quantiles up for debate
-    if (do.plot == TRUE) {
-        plot.ts.1sam(x, y, ..., funname = funname, lenx = lenx)
-    }
-    return(z)
-}
 power.res <- function(x, y, f, g = perm.test, boot = FALSE) {
     # A nice function for power calculations Takes as input a two matrices. Each row a
     # different draw of col's from the row dist.
@@ -105,45 +46,6 @@ power.res.exact <- function(x, dist, ..., f, boot = FALSE) {
     z <- pv
     return(z)
 }
-myts.par <- function(x, y, ..., interp = 4, do.plot = FALSE, size = 0.25) {
-    # Computes area based on trapezoid areas Args: x: A vector of observations for a
-    # R.V. (must be numeric) y: Either (1) Another vector of observations (two sample)
-    # (2) A quantile function such as qnorm (one sample) size: controls height of
-    # trapezoids Returns: The value of the statistic
-    
-    # Error Handling
-    if (is.numeric(x) != TRUE) 
-        stop("x must be numeric")
-    
-    x <- sort(x)
-    lenx <- length(x)
-    
-    # One Sample
-    if (is.function(y)) 
-        funname <- as.character(substitute(y))
-    if (is.character(y)) 
-        funname <- y
-    y <- get(funname, mode = "function", envir = parent.frame())
-    if (!is.function(y)) 
-        stop("'y' must be numeric or a function or a string naming a valid function")
-    # Quantiles
-    x1 <- seq(1/(lenx + 1), lenx/(lenx + 1), length.out = lenx)
-    # Parameter which determines height of trapezoids
-    delta <- size * (1/lenx)
-    # Creating quantile function q1 <- quantile(x, probs = x1, type = interp) q_inter <-
-    # approxfun(x1, q1, yleft = min(q1), yright = max(q1)) Length of horizontal segments
-    x_p_d <- x + (y(x1 + delta, ...) - y(x1, ...))
-    x_m_d <- x + (y(x1 - delta, ...) - y(x1, ...))
-    y_p_d <- y(x1 + delta, ...)
-    y_m_d <- y(x1 + delta, ...)
-    # sum of trapezoid areas
-    z <- sum((abs(x_p_d - y_p_d) + abs(x_m_d - y_m_d)) * delta)
-    if (do.plot == TRUE) {
-        plot.ts.1sam(x, y, ..., funname = funname, lenx = lenx)
-        
-    }
-    return(z)
-}
 quad.area <- function(x1, x2, y1, y2) {
     t1 <- tri.area(x1, x2, y1)
     t2 <- tri.area(x2, y1, y2)
@@ -153,51 +55,6 @@ quad.area <- function(x1, x2, y1, y2) {
 tri.area <- function(x, y, z) {
     area <- 0.5 * abs((x[1] - z[1]) * (y[2] - x[1]) - (x[1] - y[1]) * (z[2] - x[2]))
     return(area)
-}
-myts.max <- function(x, y, ...) {
-    obc.stat <- myts(x, y)
-    ks.stat <- ks.res.simp(x, y)
-    if (obc.stat > ks.stat) {
-        z <- list(obc.stat, c("OBC"))
-    } else if (ks.stat > obc.stat) {
-        z <- list(ks.stat, c("KS"))
-    } else z <- list(c(NULL), c("NEITHER"))
-    
-    return(z)
-}
-myts.max.simp <- function(x, y, ...) {
-    obc.stat <- myts(x, y, ...)
-    ks.stat <- ks.res.simp(x, y, ...)
-    if (obc.stat > ks.stat) {
-        z <- obc.stat
-    } else if (ks.stat > obc.stat) {
-        z <- ks.stat
-    } else z <- 1000  #######FIX THIS LATER
-    
-    return(z)
-}
-myts.max.range <- function(x, y) {
-    # Returns value of the statistic and whether or not it from OBC
-    com <- c(x, y)
-    range <- max(com) - min(com)
-    obc.stat <- myts(x, y)/range
-    ks.stat <- ks.res(x, y)
-    if (obc.stat > ks.stat) {
-        z <- list(obc.stat, c("OBC"))
-    } else if (ks.stat > obc.stat) {
-        z <- list(ks.stat, c("KS"))
-    } else z <- list(c(NULL), c("NEITHER"))
-    
-    return(z)
-}
-myts.max.range.simp <- function(x, y) {
-    # Returns value of the statistic and whether or not it from OBC
-    com <- c(x, y)
-    range <- max(com) - min(com)
-    obc.stat <- myts(x, y)/range
-    ks.stat <- ks.res.simp(x, y)
-    z <- max(obc.stat, ks.stat)
-    return(z)
 }
 perm.test <- function(x, y, ..., f, num.perm = 2001, diag = FALSE, exact = TRUE) {
     # Runs a permutation test for a given function (Note: Input function should JUST
