f8bb7b2a2e9319a01cc35ea2d503ee94e7417030
Mark Orndorff <markorndorff@gmail.com>
2014-07-31 14:37:36 -0400

    # Working on iterative outliers stuff (again)

--- July30_Iterative_Outliers.R
+++ July30_Iterative_Outliers.R
@@ -1,3 +1,75 @@
 # Iterative Outliers
 # Take 2
 # July 30, 2014
+# Goals: Add counter to outlier code
+source("functions.R")
+set.seed(5)
+# Did small vector for exact --------
+# x <- c(1,2,3,4,5,6,7,8,9,10)
+# y <- c(1,2,3,4,5,6,7,8,9,100)
+# 
+# new.perm.test.out(x,y,f=myts.out) #(1)
+# # expected output is: one outlier removed, 
+# # p-value high
+# new.perm.test(x,y,f=myts) #(2)
+# 
+# new.perm.test(x,y[1:9],f=myts) #(3)
+# # as of right now, there is a difference b/w (1) and (3)
+# new.perm.test.out(x,y[1:9],f=myts.out) #(4)
+# 
+# new.perm.test(x[1:9],y[1:9],f=myts) #(5)
+# 
+# # (1) and (5) are the same
+# # (3) and (4) are also the same
+# 
+# # current outlier test deletes both entries of the offending
+# # observation
+# Larger vector
+# Larger vector --------
+x <- c(1:15)
+y <- c(1:14,100)
+new.perm.test(x,y,f=myts)
+new.perm.test.out(x,y,f=myts)
+
+# Current task: idientify x or y in a non-dumb way ----------
+# temporarily set new.perm.test.out to return quan.mat
+quanmat <- new.perm.test.out(x,y,f=myts.out)
+quantable <- table(quanmat)
+str(quantable)
+attributes(quantable)
+typeof(quantable)
+sample(quantable, size=4, replace=TRUE)
+# testing out schemes
+x <- 1:10
+y <- 11:20
+names(x) <- rep("x",length(x))
+names(y) <- rep("y",length(y))
+newvec <- c(x,y)
+newsam <- sample(newvec,size=20,replace=TRUE)
+# above scheme seems promising
+tabsam <- table(newsam)
+which.max(tabsam)
+which(x == max(tabsam)) # also includes the fact that it is from x
+
+# slightly different scheme:
+x <- 1:10
+y <- 1:10
+names(x) <- rep("x",length(x))
+names(y) <- rep("y",length(y))
+newvec <- c(x,y)
+newsam <- sample(newvec,size=20,replace=TRUE)
+lisam <- as.list(newsam)
+# above scheme seems promising
+tabsam <- table(newsam)
+which.max(tabsam)
+which(x == max(tabsam))
+tablisam <- table(lisam)
+
+newmat <- matrix(c(names(x),x),nrow=2,ncol=length(x), byrow=TRUE)
+as.list(newmat)
+
+test <- lapply(x,function(x) list(x,"x"))
+test
+str(test)
+samtest <- sample(test, size=15, replace=TRUE)
+table(samtest[][[]][1])
--- functions_new.R
+++ functions_new.R
@@ -2,14 +2,11 @@
 # Still using myts.out
 new.perm.test.out <- function(x,y, ..., f, fops=NULL, 
                               distops=NULL, num.perm = 2001, diag = FALSE, exact = FALSE) {
-  lenx <- length(x)
-    # Handing possibly necessary variable transformations
+  # Variable Transforms and calculating observed TS
   if (is.function(y)) 
     y <- as.character(substitute(y))
-
   if (is.character(y)) {
     y <- chartoli(y)
-    # Calculating observed test statistic
     if (length(fops) == 0) 
       fops <- NULL
     if (length(distops) == 0) 
@@ -17,16 +14,15 @@ new.perm.test.out <- function(x,y, ..., f, fops=NULL,
     ts.obs <- do.call(f, c(list(x), list(names(y)), distops, fops))[[1]]
   }
   if (is.numeric(y)){
-    # Calculating observed test statistic
     ts.obs <- do.call(f, list(x,y, fops))[[1]]
   }
   
   # Calculating distribution of test statistic
   ts.random <- vector(mode = "numeric", length = num.perm)
-  
+  lenx <- length(x)
   z <- c(x,y)
   lenz <- length(z)
-  
+  # While doing this, quan.mat also keeps track of the indexes
   quan.mat <- matrix(data = NA, nrow = num.perm, ncol = 2)
   for (i in 1:num.perm) {
     z1 <- sample(z, size = lenz, replace = FALSE)
@@ -39,7 +35,7 @@ new.perm.test.out <- function(x,y, ..., f, fops=NULL,
     ord <- res[[2]]
     quan.mat[i, ] <- c(a[ord], b[ord])
   }
-  
+  return(quan.mat)
   # Tabulating the values of quan.mat
   tab.quan.mat <- table(quan.mat)
   # Getting the number most repeated
